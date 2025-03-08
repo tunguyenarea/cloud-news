ee9f4518b0b85a0c631b66857c784cb609ab891d
tunguyenarea <tunguyenarea@gmail.com>
2025-03-08 10:15:15 +0700

    Commit 3

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -11,7 +11,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 
   <main>
     <SectionFive id={id}/>
-    <div className="grid gap-6 md:flex md:justify-around m-6 md:m-12">
+    <div className="grid gap-6 md:flex md:justify-between m-6 md:m-8">
       <Link href="/profile">
         <button className="bg-black dark:bg-white rounded-lg text-white dark:text-black p-2 w-full md:w-36 h-10 hover:bg-gradient-to-r hover:from-red-500 hover:via-orange-500 hover:to-yellow-500 hover:text-white">Back</button>
       </Link>
